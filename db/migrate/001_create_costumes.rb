faf781f218eeff8e41b13bdd665319d25bb5dac7
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-02-15 17:20:30 -0500

    commit tree 4162e000eadc3500c5de0293e2eb32ff165ac65d

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,4 +1,15 @@
 # Create a class and inherit from ActiveRecord::Migration
+class CreateCostumes < ActiveRecord::Migration[4.2]
+    def change
+        create_table :costumes do |t|
+            t.string :name
+            t.float :price
+            t.string :image_url
+            t.string :size
+            t.timestamps
+        end
+    end
+end
 
 # By convention, the class name should match the part of the
 # file name after the number, so in this case:
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,3 +1,17 @@
 # Create your haunted_houses migration here
-
+class CreateHauntedHouses < ActiveRecord::Migration[4.2]
+    def change
+        create_table :haunted_houses do |t|
+            t.string :name
+            t.string :location
+            t.string :theme
+            t.float :price
+            t.boolean :family_friendly
+            t.text :opening_date
+            t.text :closing_date
+            t.text :description
+        end
+    end
+end
+# !!! Bef
 # !!! Before you run rake db:migrate, remember to fill out the other migration files -- otherwise you'll get an error resulting from the blank migration files.
